22d631e5a4bc87a94237ec3709e94c34a4bc7bf2
SimonIT <simonit.orig@gmail.com>
2019-09-09 11:19:30 +0200

    some test classes

---
+++ src/test/java/de/szut/simNil/binaryMaple/AbstractNodeTest.java
@@ -0,0 +1,20 @@
+package de.szut.simNil.binaryMaple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractNodeTest {
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
