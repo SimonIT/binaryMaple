4513b9063ee44cd1f77fb095ab2be763890648d7
nils407 <nils407@schule.bremen.de>
2019-09-24 16:49:43 +0200

    updated StandardBinarySearchTree

--- src/main/java/de/szut/simNil/binaryMaple/AbstractNode.java
+++ src/main/java/de/szut/simNil/binaryMaple/AbstractNode.java
@@ -2,17 +2,9 @@ package de.szut.simNil.binaryMaple;
 
 import org.jetbrains.annotations.Nullable;
 
-/**
- * Astract class for a node
- * Takes any comparable as Value
- *
- * @author wolke
- * @version 1.1, 19.08.2019
- */
-
 public abstract class AbstractNode<T extends Comparable<T>> {
     @Nullable
-    protected T value; // integer value
+    protected T value;
 
     public AbstractNode() {
     }
@@ -30,18 +22,7 @@ public abstract class AbstractNode<T extends Comparable<T>> {
         this.value = value;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof AbstractNode) {
-            if (this.getValue() != null && ((AbstractNode) obj).getValue() != null) {
-                return ((AbstractNode) obj).getValue().equals(this.getValue());
-            } else return this.getValue() == null && ((AbstractNode) obj).getValue() == null;
-        }
-        return false;
-    }
-
     public String toString() {
         return getValue() != null ? getValue().toString() : "null";
     }
-
 }
--- src/main/java/de/szut/simNil/binaryMaple/InterfaceBinarySearchTree.java
+++ src/main/java/de/szut/simNil/binaryMaple/InterfaceBinarySearchTree.java
@@ -1,51 +1,25 @@
 package de.szut.simNil.binaryMaple;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-/**
- * Interface fuer Binary Search Tress
- *
- * @author wolke
- * @version 1.1, 29.08.2019
- */
+
 public interface InterfaceBinarySearchTree<T extends Comparable<T>> {
+    void addValue(T value) throws BinarySearchTreeException;
 
-    /**
-     * @param value the value we'd like to add
-     * @throws BinarySearchTreeException should usually not occur
-     */
-    void addValue(T value)
-        throws BinarySearchTreeException;
+    void delValue(@NotNull T value) throws BinarySearchTreeException;
 
-    /**
-     * @param value the value we'd like to delete
-     * @throws BinarySearchTreeException If the value is not in the tree.
-     */
-    void delValue(T value)
-        throws BinarySearchTreeException;
-
-    /**
-     * @param value The value we are searching
-     * @return True or False(we couldn't find it)
-     */
-    boolean hasValue(T value);
+    boolean hasValue(@NotNull T value);
 
     @Nullable
-    AbstractNode<T> getNodeWithValue(T value);
+    AbstractNode<T> getNodeWithValue(@NotNull T value);
 
-    /**
-     * @return height of the tree
-     */
     default Integer getDepth() {
         return null;
     }
 
-    /**
-     * @return list with all values in correct order
-     * LEVELORDER not NECCESARY
-     */
     default List<T> traverse(Order o) {
         return null;
     }
