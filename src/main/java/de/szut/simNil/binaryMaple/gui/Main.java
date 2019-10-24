aa724ce0382feabe6f8cb7b3774fed09d0726a23
SimonIT <simonit.orig@gmail.com>
2019-10-24 11:38:56 +0200

    Alt+E to insert values

--- src/main/java/de/szut/simNil/binaryMaple/gui/Main.java
+++ src/main/java/de/szut/simNil/binaryMaple/gui/Main.java
@@ -4,11 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static final KeyCombination[] combinations = new KeyCombination[]{
+        new KeyCodeCombination(KeyCode.E, KeyCombination.ALT_DOWN),
+    };
 
     public static void main(String[] args) {
         launch(args);
@@ -18,10 +26,18 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("style.fxml"));
         Parent root = loader.load();
-        ((Controller) loader.getController()).setStage(stage);
+        Controller controller = loader.getController();
+        controller.setStage(stage);
         stage.setTitle("BinaryMaple");
         stage.setMaximized(true);
         stage.setScene(new Scene(root));
         stage.show();
+
+        stage.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+            if (combinations[0].match(keyEvent)) {
+                controller.addValue();
+                keyEvent.consume();
+            }
+        });
     }
 }
